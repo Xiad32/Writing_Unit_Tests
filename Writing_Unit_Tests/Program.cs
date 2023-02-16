a9e8b6516124e09acd1befbdb4e4391bf615428e
Xiad32 <xiadaw@aucegypt.edu>
2023-02-15 19:27:43 -0700

    manual debugging

---
+++ Writing_Unit_Tests/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using CodingStandardPractice;
+
+float[] average = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+float[] sample =  { 1, 1, 1, 1, 1, 1, 1, 1, 1,  1 };
+var result = Algorithm.AccumilateArrays(average, sample);
